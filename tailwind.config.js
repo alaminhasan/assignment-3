1be941a8197300a32928f44fa5020ce93b0bac71
Alamin Hasan <alaminhasan710@gmail.com>
2025-06-28 16:49:49 +0600

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}", // adjust according to your project structure
+    ],
+    theme: {
+        extend: {
+           
+        },
+    },
+    plugins: [],
+}
